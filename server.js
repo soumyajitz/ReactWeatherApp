00f8a7e2575324380bec8124198025cc97a6ef65
soumyajitz <2014soumyajit@gmail.com>
2017-01-28 22:56:11 -0600

    fixing localhost error

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const PORT = process.env.PORT || 3000;
 // Create the app
 var app = express();
 
-// app.use(function(req,res,next) {
-//     if(req.headers['x-forwarded-proto'] === 'http') {
-//         next();
-//     } else {
-//         res.redirect('http://' + req.hostname + req.url);
-//     }
-// });
+app.use(function(req,res,next) {
+    if(req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();
+    }
+});
 
 app.use(express.static('public'));
 
