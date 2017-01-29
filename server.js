cb4d4e0bd1072d01155f03bd0ce5a304734e3af1
soumyajitz <2014soumyajit@gmail.com>
2017-01-28 22:10:55 -0600

    feat(heroku): added server config for heroku

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
 
+
+const PORT = process.env.PORT || 3000;
 // Create the app
 var app = express();
+
+app.use(function(req,res,next) {
+    if(req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-    console.log('Express is up on 3000');
+app.listen(PORT, function(){
+    console.log('Express is up on port:'+ PORT);
 });
